d36404b0b3cc1c93661c017326c4bbfe1c0eca42
tamagoage <kokuto0512@gmail.com>
2025-08-13 14:25:00 +0900

    update 03 Image型をつけた

--- 03_random_cat/app/fetch-image.ts
+++ 03_random_cat/app/fetch-image.ts
@@ -1,5 +1,10 @@
+// 画像情報の形定義
+type Image = {
+    url: string;
+}
+
 // APIから画像を取得する関数
-export async function fetchImage() {
+export async function fetchImage(): Promise<Image> {
     const response = await fetch("https://api.thecatapi.com/v1/images/search");
     const images = await response.json();
     console.log("画像情報を取得しました", images);
