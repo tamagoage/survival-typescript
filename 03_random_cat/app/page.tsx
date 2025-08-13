47af555854b06e3f1fb2f0f008fc31dceca45c3d
tamagoage <kokuto0512@gmail.com>
2025-08-13 14:34:39 +0900

    update 03 トップページアクセス時、猫の画像を表示するようにした

--- 03_random_cat/app/page.tsx
+++ 03_random_cat/app/page.tsx
@@ -1,9 +1,9 @@
 import { connection } from "next/server";
 import { fetchImage } from "./fetch-image";
+import { CatImage } from "./cat-image";
 
 export default async function Home() {
   await connection();
   const image = await fetchImage();
-  console.log("画像情報を取得しました。", image);
-  return <div>猫画像予定地</div>;
-}
+  return <CatImage url={image.url} />
+};
